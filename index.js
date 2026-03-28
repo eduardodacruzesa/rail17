eac45e14e287c3ed4a86fdc385d439d9831b45f9
eduardodacruzesa <eduardo.cruz.sa@gmail.com>
2026-03-28 11:05:24 -0300

    atencao_Eduardo

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send('Hello World 7 7!');
+=======
+  res.send('Hello World!');
+>>>>>>> f55e3e2 (Meu primeiro commit)
 });
 
 app.listen(PORT, () => {
