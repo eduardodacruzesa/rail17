24bb5c9c6b6146347634dd81079eb67ca099458b
eduardodacruzesa <31220994+eduardodacruzesa@users.noreply.github.com>
2026-03-27 22:47:04 -0300

    Update index.js
    colocando 7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World 7 7!');
 });
 
 app.listen(PORT, () => {
