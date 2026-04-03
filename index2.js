02e5ffc694f58537d6f5462a7c60e5dbc2ff9997
eduardodacruzesa <eduardo.cruz.sa@gmail.com>
2026-04-03 11:22:07 -0300

    a primeira tentativa nao funcionou

--- index2.js
+++ index2.js
@@ -3,8 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World setenta vezes sete 7777777!');
-  res.send('Agora uma segunda linha, só para ver, kkk');
+  res.send('Hello World setenta vezes sete 7777777!    -- mas agora estou mandando mais alguma coisa');
 });
 
 app.listen(PORT, () => {
