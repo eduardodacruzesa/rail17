d70f1f3d87649919312bd2191796287e3a17ad28
eduardodacruzesa <eduardo.cruz.sa@gmail.com>
2026-04-03 14:03:07 -0300

    agora com vários H´s

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World setenta vezes sete 7777777!   <h2> -- mas agora estou mandando mais alguma </h2> coisa');
+  res.send('Hello World setenta vezes sete 7777777!   <h2> -- mas agora estou mandando mais alguma </h2> <h3>este é H3</h3><p><h4>este é H4</h4></p> coisa');
 });
 
 app.listen(PORT, () => {
